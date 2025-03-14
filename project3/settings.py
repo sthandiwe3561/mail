138e8ddf80a62a3bc26030caf0621758ffec57a6
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-03-14 18:28:17 +0200

    for frontend

--- project3/settings.py
+++ project3/settings.py
@@ -86,6 +86,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project3.wsgi.application'
 
+# CORS settings (Allow frontend to access backend)
+CORS_ALLOWED_ORIGINS = [
+    "https://your-frontend.onrender.com"
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
